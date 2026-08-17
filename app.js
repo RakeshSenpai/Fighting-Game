19418bd0776ae9b5b37e59be331a41d724b55271
Rakesh07778777 <bossrakesh43@gmail.com>
2026-08-17 23:53:09 +0530

    player velocity pressing keys

--- app.js
+++ app.js
@@ -10,20 +10,29 @@ c.fillRect(0,0, canvas.width, canvas.height);
 
 
 //Class
+
+const gravity = 0.2
 class Sprite{
     constructor({position, velocity}){
         this.position = position;
         this.velocity = velocity
+        this.height = 150
     }
     draw(){
         c.fillStyle = 'red';
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
     }
 
     update(){
         this.draw();
-        this.position.y = this.position.y + 10;
-        this.position.y += 10;
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        if(this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0;
+        } else{
+            this.velocity.y += gravity
+        }
          
     }
 }
@@ -60,12 +69,50 @@ const enemy = new Sprite({
 
 console.log(player);
 
+const keys = {
+    a:{
+        pressed: false
+    }, 
+    d: {
+        pressed : false
+    }
+}
+
 function animate(){
     requestAnimationFrame(animate);
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
     enemy.update()
+
+    if(keys.a.pressed){
+        player.velocity.x = -1
+    } else if(keys.d.pressed){
+        player.velocity.x = 1;
+    }
 }
  
 animate()
+
+window.addEventListener('keydown' , (e) => {
+    switch (e.key){
+        case 'd':
+            keys.d.pressed = true
+        break;
+        case 'a':
+            keys.a.pressed = true
+            break
+    }
+
+})
+window.addEventListener('keyup' , (e) => {
+    switch (e.key){
+        case 'd':
+            keys.d.pressed = false
+        break;
+        case 'a':
+            keys.a.pressed = false
+            break
+    }
+
+})
